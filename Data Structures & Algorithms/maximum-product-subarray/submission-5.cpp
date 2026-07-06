25e777933f3c87c8b720b445c5f0b61b71650500
yeosun02 <44177734+yeosun02@users.noreply.github.com>
2026-07-05 21:01:39 -0700

    Add: maximum-product-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int max_val = nums[0], min_val = nums[0], res = nums[0];
+
+        for (int i = 1; i < nums.size(); ++i) {
+            cout << nums[i] << " " << max_val << " " << min_val << endl; 
+            int temp = max_val;
+            max_val = max({nums[i], nums[i] * max_val, nums[i] * min_val});
+            min_val = min({nums[i], nums[i] * temp, nums[i] * min_val});
+            res = max(res, max_val);
+        }
+
+        return res;
+    }
+};
