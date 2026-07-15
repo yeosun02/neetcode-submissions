be8241b0af31256567b8476645220366829377eb
yeosun02 <44177734+yeosun02@users.noreply.github.com>
2026-07-15 14:44:55 -0700

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [False] * n
+        dp[-1] = True
+        closest = n - 1
+        for i in range(n - 2, -1, -1):
+            if nums[i] + i >= closest:
+                dp[i] = True
+                closest = i
+        
+        return nums[0] >= closest
